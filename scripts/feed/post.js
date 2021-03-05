9e50f1465ee7105ffde47e110494b976b641e16d
Laurel Morrison <lbentleym1992@gmail.com>
2021-03-05 16:27:29 -0500

    adding CSS

--- scripts/feed/post.js
+++ scripts/feed/post.js
@@ -2,9 +2,9 @@ export const Post = (postObject) => {
     return `
       <section class="post">
         <header>
-            <h2 class="post__title">${postObject.title}</h2>
+            <h3 class="postTitle">${postObject.title}</h3>
         </header>
-        <img class="post__image" src="${postObject.url}" />
+        <img class="postImage" src="${postObject.url}" />
         <p class="explanation">Explanation: ${postObject.explanation}</p>
         <p>Date: ${postObject.date}</p>
         <p>Copyright: ${postObject.copyright}</p>
